1e9d0fd02243f31d4e224e238d3e1a2b88001c52
Bansi Dobariya <148076698+bansi1008@users.noreply.github.com>
2025-09-27 14:57:50 +0100

    Time: 2 ms (91.43%), Space: 41.1 MB (92.77%) - LeetHub

---
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
@@ -0,0 +1,61 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int sum = 0;
+        for (int num : nums) sum += num;
+
+        if (sum % k != 0) return false;
+
+        int target = sum / k;
+
+        Arrays.sort(nums);
+        reverse(nums);
+
+        int[] buckets = new int[k];
+        return backtrack(nums, 0, buckets, target);
+    }
+
+    private boolean backtrack(int[] nums, int index, int[] buckets, int target) {
+        if (index == nums.length) {
+            for (int b : buckets) {
+                if (b != target) return false;
+            }
+            return true;
+        }
+
+        int num = nums[index];
+
+        for (int i = 0; i < buckets.length; i++) {
+            if (buckets[i] + num <= target) {
+                buckets[i] += num;
+                if (backtrack(nums, index + 1, buckets, target)) {
+                    return true;
+                }
+                buckets[i] -= num; 
+            }
+
+            if (buckets[i] == 0) break;
+        }
+
+        return false;
+    }
+
+    private void reverse(int[] nums) {
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            int temp = nums[i]; nums[i] = nums[j]; nums[j] = temp;
+            i++; j--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] nums1 = {4,3,2,3,5,2,1};
+        System.out.println(sol.canPartitionKSubsets(nums1, 4)); 
+
+        int[] nums2 = {1,2,3,4};
+        System.out.println(sol.canPartitionKSubsets(nums2, 3)); 
+    }
+}
