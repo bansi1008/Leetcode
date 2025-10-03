92155140ea516fc123527ee3e2ded7065aa930a0
Bansi Dobariya <148076698+bansi1008@users.noreply.github.com>
2025-10-03 09:48:07 +0100

    Time: 17 ms (95.02%), Space: 46.4 MB (60.86%) - LeetHub

---
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.java
@@ -0,0 +1,63 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    class Cell implements Comparable<Cell> {
+        int x, y, height;
+        Cell(int x, int y, int height) {
+            this.x = x;
+            this.y = y;
+            this.height = height;
+        }
+        
+        @Override
+        public int compareTo(Cell other) {
+            return this.height - other.height;
+        }
+    }
+    
+    public int trapRainWater(int[][] heightMap) {
+        if (heightMap == null || heightMap.length == 0 || heightMap[0].length == 0) {
+            return 0;
+        }
+        
+        int m = heightMap.length;
+        int n = heightMap[0].length;
+        boolean[][] visited = new boolean[m][n];
+        PriorityQueue<Cell> pq = new PriorityQueue<>();
+        
+    
+        for (int i = 0; i < m; i++) {
+            pq.offer(new Cell(i, 0, heightMap[i][0]));
+            pq.offer(new Cell(i, n - 1, heightMap[i][n - 1]));
+            visited[i][0] = true;
+            visited[i][n - 1] = true;
+        }
+        
+        for (int j = 1; j < n - 1; j++) {
+            pq.offer(new Cell(0, j, heightMap[0][j]));
+            pq.offer(new Cell(m - 1, j, heightMap[m - 1][j]));
+            visited[0][j] = true;
+            visited[m - 1][j] = true;
+        }
+        
+        int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
+        int water = 0;
+        
+        while (!pq.isEmpty()) {
+            Cell cell = pq.poll();
+            
+            for (int[] dir : dirs) {
+                int nx = cell.x + dir[0];
+                int ny = cell.y + dir[1];
+                
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n && !visited[nx][ny]) {
+                    water += Math.max(0, cell.height - heightMap[nx][ny]);
+                    visited[nx][ny] = true;
+                    pq.offer(new Cell(nx, ny, Math.max(heightMap[nx][ny], cell.height)));
+                }
+            }
+        }
+        
+        return water;
+    }
+}
