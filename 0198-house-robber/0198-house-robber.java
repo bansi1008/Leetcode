cf7d90e367be543772dc25b18513bb04eeffa9fa
Bansi Dobariya <148076698+bansi1008@users.noreply.github.com>
2025-10-06 11:17:56 +0100

    Time: 0 ms (100%), Space: 41.1 MB (47.82%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -3,17 +3,22 @@ class Solution {
         if(nums.length<=1){
             return nums[0];
         }
-        int[] dp=new int[nums.length];
+      
 
 
-        dp[0]=nums[0];
-        dp[1]=Math.max(nums[0],nums[1]);
+        int prev2=nums[0];
+        int prev1 = Math.max(nums[0], nums[1]);
+
+        int curr=0;
 
         for(int i=2;i<nums.length;i++){
-            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+            curr=Math.max(nums[i]+prev2,prev1);
+             prev2=prev1;
+            prev1=curr;
+           
         }
 
-return dp[nums.length-1];
+return prev1;
         
     }
 }
