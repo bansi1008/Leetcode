f13e8307873505f860e374115bb0ca6341f52000
Bansi Dobariya <148076698+bansi1008@users.noreply.github.com>
2025-10-07 14:02:26 +0100

    Time: 14 ms (49.22%), Space: 45.2 MB (47.03%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,29 +1,37 @@
 class Solution {
-    public int minEatingSpeed(int[] piles, int h) {
-        int left = 1; 
-        int right = 0; 
-        for (int pile : piles) {
-            right = Math.max(right, pile);
+    public static long hours(int[] piles, int k) {
+        long h = 0;
+        for (int p : piles) {
+
+            h += (p +(long) k - 1) / k;
         }
-
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-
-            if (canFinish(piles, h, mid)) {
-                right = mid; 
-            } else {
-                left = mid + 1; 
-            }
-        }
-
-        return left; 
+        return h;
     }
 
-    private boolean canFinish(int[] piles, int h, int k) {
-        int hours = 0;
-        for (int pile : piles) {
-            hours += (pile + k - 1) / k; 
+    public int minEatingSpeed(int[] piles, int h) {
+        Arrays.sort(piles);
+
+        int l = 1;
+        int hi = 0;
+
+        for (int p : piles) {
+            hi = Math.max(p, hi);
         }
-        return hours <= h;
+        int res = hi;
+        while (l <= hi) {
+            int mid = l + (hi - l) / 2;
+
+            long hourss = hours(piles, mid);
+            if (hourss <= h) {
+                res = mid;
+                hi = mid - 1;
+
+            } else {
+                l = mid + 1;
+            }
+
+        }
+        return res;
+
     }
 }
