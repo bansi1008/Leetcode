5f60a9ce11549c65cc924d31aab4dbbf1f7b1d02
Bansi Dobariya <148076698+bansi1008@users.noreply.github.com>
2025-10-09 11:18:08 +0100

    Time: 0 ms (100%), Space: 41.6 MB (82.49%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -2,25 +2,36 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m=obstacleGrid.length;
        int n=obstacleGrid[0].length;
-       int[][] dp=new int[m][n];
+       int[] dp=new int[n+1];
+       int c=0;
+       int prevc=0;
+       int prevr=0;
+
+       Arrays.fill(dp,0);
 
        for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
             if(obstacleGrid[i][j]==1){
+                dp[j] = 0;
                 continue;
             }
             if(i==0 && j==0){
-                dp[i][j]=1;
+              dp[j]=1;
+               
+
                 continue;
             }
-            int up=(i>0) ?dp[i-1][j]:0;
-            int left=(j>0)?dp[i][j-1]:0;
+            int up=(j>0) ?dp[j-1]:0;
+            int left=dp[j];
 
-            dp[i][j]=up+left;
+            c=up+left;
+            dp[j]=c;
+
         }
+        
        }
        
-   return dp[m-1][n-1];
+   return dp[n-1];
 }
 }
 
@@ -46,5 +57,31 @@ class Solution {
 
 
     }
+
+
+        int m=obstacleGrid.length;
+       int n=obstacleGrid[0].length;
+       int[][] dp=new int[m][n];
+       int c=0;
+       int prevrc=0
+       int prevr=0
+
+       for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(obstacleGrid[i][j]==1){
+                continue;
+            }
+            if(i==0 && j==0){
+                dp[i][j]=1;
+                continue;
+            }
+            int up=(i>0) ?dp[i-1][j]:0;
+            int left=(j>0)?dp[i][j-1]:0;
+
+            dp[i][j]=up+left;
+        }
+       }
+       
+   return dp[m-1][n-1];
     */
     
