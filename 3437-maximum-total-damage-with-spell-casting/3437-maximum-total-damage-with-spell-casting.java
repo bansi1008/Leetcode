904993b7c9e5b5bc35de9cfe7f7af3610fd9bddc
Bansi Dobariya <148076698+bansi1008@users.noreply.github.com>
2025-10-11 12:42:02 +0100

    Time: 53 ms (100%), Space: 61.7 MB (75.22%) - LeetHub

--- 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.java
+++ 3437-maximum-total-damage-with-spell-casting/3437-maximum-total-damage-with-spell-casting.java
@@ -1,35 +1,33 @@
 class Solution {
     public long maximumTotalDamage(int[] power) {
         Arrays.sort(power);
-        
-        TreeMap<Long, Long> freq = new TreeMap<>();
+         int n=power.length;
 
-        for(int val:power){
-            freq.put((long) val, freq.getOrDefault((long) val, 0L) + 1);
+       long[] dp=new long[n];
+       dp[0]=power[0];
+       long md=0;
+
+       for(int i=1,j=0;i<n;i++){
+        if(power[i]==power[i-1]){
+            dp[i]=power[i]+dp[i-1];
+        }
+        else{
+            while(power[j]+2<power[i]){
+                md=Math.max(md,dp[j]);
+                j++;
+            }
+            dp[i]=md+power[i];
         }
 
-        List<Long> keys = new ArrayList<>(freq.keySet());
-        int n = keys.size();
-        long[] dp = new long[n];
-        dp[0] = keys.get(0) * freq.get(keys.get(0));
-
-         int p = -1;
-        for (int i = 1; i < n; i++) {
-            long curr = keys.get(i);
-            long currval = curr * freq.get(curr);
-
-            while (p + 1 < i && keys.get(p + 1) < curr - 2)
-                p++;
-
-            long in = currval + (p >= 0 ? dp[p] : 0);
-            long ex = dp[i - 1];
-            dp[i] = Math.max(in, ex);
-        }
-        return dp[n - 1];
+       }
 
 
+     long max=0;
 
-      
+      for(long num:dp){
+        max=Math.max(max,num);
+      }
+      return max;
 
         
     }
