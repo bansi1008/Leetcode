1015a4bcecc4abe191e0fe9550057015b4b90246
Bansi Dobariya <148076698+bansi1008@users.noreply.github.com>
2025-10-15 12:22:36 +0100

    Time: 35 ms (83.95%), Space: 44.7 MB (48.77%) - LeetHub

--- 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.java
+++ 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.java
@@ -3,11 +3,16 @@ class Solution {
         int n=(hi-lo)+1;
 
          PriorityQueue<int[]> pq=new PriorityQueue<>((a, b) -> a[1] == b[1] ? a[0] - b[0] : a[1] - b[1]);
+         Map<Integer, Integer> cache = new HashMap<>();
 
          for(int i=lo;i<=hi;i++){
             int c=0;
             int val=i;
             while(val!=1){
+                if(cache.containsKey(val)){
+                    c+=cache.get(val);
+                    break;
+                }
                if(val%2==0){
                 val /=2;
                 c++;
@@ -17,6 +22,7 @@ class Solution {
                 c++;
                }
             }
+            cache.put(i,c);
             pq.add(new int[]{i,c});
          }
 
