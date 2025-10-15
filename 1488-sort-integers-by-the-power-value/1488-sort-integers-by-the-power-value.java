8a3b5929cc64de10b8a600977ebf90ba7fd59e1f
Bansi Dobariya <148076698+bansi1008@users.noreply.github.com>
2025-10-15 12:19:50 +0100

    Time: 31 ms (90.43%), Space: 43.4 MB (95.06%) - LeetHub

---
+++ 1488-sort-integers-by-the-power-value/1488-sort-integers-by-the-power-value.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int getKth(int lo, int hi, int k) {
+        int n=(hi-lo)+1;
+
+         PriorityQueue<int[]> pq=new PriorityQueue<>((a, b) -> a[1] == b[1] ? a[0] - b[0] : a[1] - b[1]);
+
+         for(int i=lo;i<=hi;i++){
+            int c=0;
+            int val=i;
+            while(val!=1){
+               if(val%2==0){
+                val /=2;
+                c++;
+               }
+               else{
+                val=val*3+1;
+                c++;
+               }
+            }
+            pq.add(new int[]{i,c});
+         }
+
+            int j=0;
+            int curr=0;
+            while(j < k) {
+    int[] v = pq.poll();
+    curr = v[0];
+    j++;
+}
+            return curr;
+    }
+}
