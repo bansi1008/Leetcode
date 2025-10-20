a4c13c20bcd26111187990db7ae34d8c608a8af1
Bansi Dobariya <148076698+bansi1008@users.noreply.github.com>
2025-10-20 09:39:52 +0100

    Time: 38 ms (20.51%), Space: 55.7 MB (86.5%) - LeetHub

---
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+       
+        int c=0;
+       
+        int h=nums.length;
+        int sum=0;
+               Map<Integer, Integer> map = new HashMap<>();
+
+       
+
+        for(int i=0;i<nums.length;i++){
+          sum=k-nums[i];
+
+           if (map.getOrDefault(sum, 0) > 0) {
+                c++;
+                map.put(sum, map.get(sum) - 1);
+            } else {
+                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            }
+
+        }
+        return c;
+    }
+}
